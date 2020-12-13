5602a7f0cf8d95a3461d245d3f312d9e1b6fb8da
Knair <knairwang@outlook.com>
2020-12-13 22:47:22 +0800

    event page: process more key event, add event detail view

--- src/view/component/message_component.rs
+++ src/view/component/message_component.rs
@@ -12,26 +12,43 @@ impl From<Rc<MessageComponentViewModel>> for MessageComponent {
 
 impl Widget for &MessageComponent {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        Block::default()
+        let mut border = Block::default()
             .borders(Borders::ALL)
-            .border_type(border_normal_type())
-            .render(area, buf);
+            .border_type(border_normal_type());
+        let title = self.vm.title.borrow();
+        if let Some(title) = title.as_deref() {
+            border = border.title(format!(" {} ", title));
+        }
+        border.render(area, buf);
 
-        if let Some(text) = self.vm.text.borrow().as_deref() {
-            let text_area = Rect::new(area.x + 4, area.y + 2, area.width - 8, area.height - 4);
-            Paragraph::new(text).render(text_area, buf);
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(vec![Constraint::Length(1), Constraint::Min(1)])
+            .split(area);
+
+        if let Some(text) = self.vm.subject.borrow().as_deref() {
+            Paragraph::new(format!("Subject: {}", text)).style(subject_text_style()).render(chunks[0], buf);
+        }
+
+        if let Some(text) = self.vm.content.borrow().as_deref() {
+            Paragraph::new(text).style(content_text_style()).render(chunks[1], buf);
         }
     }
 }
 
 pub struct MessageComponentViewModel {
-    pub text: RefCell<Option<String>>,
+    pub title: RefCell<Option<String>>,
+    pub subject: RefCell<Option<String>>,
+    pub content: RefCell<Option<String>>,
 }
 
 impl Default for MessageComponentViewModel {
     fn default() -> Self {
         MessageComponentViewModel {
-            text: RefCell::new(None),
+            title: RefCell::new(None),
+            subject: RefCell::new(None),
+            content: RefCell::new(None),
         }
     }
 }
