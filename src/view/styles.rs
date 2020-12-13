3283d719cacbc004731f536ffa291dab667a67e8
Knair <knairwang@outlook.com>
2020-12-13 22:43:38 +0800

    styles: add shared styles

--- src/view/styles.rs
+++ src/view/styles.rs
@@ -15,3 +15,16 @@ pub fn selection_highlight_style() -> Style {
 pub fn selection_normal_style() -> Style {
     Style::default().fg(Color::Black).bg(Color::White)
 }
+
+pub fn subject_text_style() -> Style {
+    Style::default()
+        .add_modifier(Modifier::BOLD | Modifier::UNDERLINED)
+        .bg(Color::White)
+        .fg(Color::Black)
+}
+
+pub fn content_text_style() -> Style {
+    Style::default()
+        .bg(Color::Black)
+        .fg(Color::Gray)
+}
